6957655794a441730a5cfd758f7971ee0263469c
Wanchalerm <tickstudiu@gmail.com>
2019-09-19 15:20:40 +0700

    update modal in card

--- src/components/card.component.js
+++ src/components/card.component.js
@@ -1,27 +1,57 @@
 import React from 'react';
 import Moment from 'react-moment';
+import WarningImage from '../warning.png';
+
 import {
     Card, CardText, CardBody,
-    CardTitle,
+    CardTitle, Button, Modal, ModalBody, ModalHeader
 } from 'reactstrap';
 
-const card = (props) => {
-    return (
-        <Card className="hover-shadow mb-4">
-            <CardBody>
-                <div className="d-flex justify-content-between">
-                    <CardTitle><a href="" onClick={() => props.AddRoute(props.id)}>{props.title}</a></CardTitle>
-                    <small className="text-danger cursor-pointer" onClick={() => props.deleteBib(props.id)}>delete</small>
-                </div>
-                <h6 className="text-capitalize">Checkpoint: {props.checkpoint}</h6>
-                <h6 className="text-capitalize">distance: {props.distance}</h6>
-                <CardText>{props.description}</CardText>
-                <CardText>
-                    <small className="text-muted"> <Moment fromNow>{props.date}</Moment></small>
-                </CardText>
-            </CardBody>
-        </Card>
-    );
-};
+export default class card extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            modal: false
+        };
 
-export default card;
+        this.toggle = this.toggle.bind(this);
+    }
+
+    toggle() {
+        this.setState(prevState => ({
+            modal: !prevState.modal
+        }));
+    }
+
+    render(){
+        return (
+            <Card className="hover-shadow mb-4">
+                <CardBody>
+                    <div className="d-flex justify-content-between">
+                        <CardTitle className="text-primary cursor-pointer" onClick={() => this.props.AddRoute(this.props.id)}>{this.props.title}</CardTitle>
+                        <small className="text-danger cursor-pointer" onClick={this.toggle}>delete</small>
+                    </div>
+                    <h6 className="text-capitalize">Checkpoint: {this.props.checkpoint}</h6>
+                    <h6 className="text-capitalize">distance: {this.props.distance}</h6>
+                    <CardText>{this.props.description}</CardText>
+                    <CardText>
+                        <small className="text-muted"> <Moment fromNow>{this.props.date}</Moment></small>
+                    </CardText>
+                </CardBody>
+                <Modal isOpen={this.state.modal} toggle={this.toggle} centered>
+                    <ModalBody className="text-center">
+                        <section className="mb-4">
+                            <img src={WarningImage} alt="warning image" height="100px" width="100px"/>
+                            <h3 className="my-4">Are you sure ?</h3>
+                            Do you really want to delete these records ? this process cannot be undone.
+                        </section>
+                        <section>
+                            <Button color="secondary" onClick={this.toggle} className="mr-3">Cancel</Button>
+                            <Button color="danger" onClick={() => this.props.deleteBib(this.props.id)}>Delete</Button>
+                        </section>
+                    </ModalBody>
+                </Modal>
+            </Card>
+        );
+    }
+};
